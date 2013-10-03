db76f0ee051506302be111d59753beed285dafa4
nobody01 <radekludacka@gmail.com>
2013-10-03 12:39:26 +0200

    added more robust behaviour and Release compilation

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
 #! /bin/bash
 
-make CONG=Release
-mkdir bin
-cd bin
-cp ../dist/Release/GNU-Linux-x86/lfc-analyzer ./
-cd ..
-
+make -f nbproject/Makefile-Release.mk dist/Release/GNU-Linux-x86/lfc-analyzer 
+if [[ ! -d ./bin ]]; then
+    mkdir bin || { echo "Cannot create directory bin"; return -1; }
+fi
+cd bin || { echo "Directory bin does not exist"; return -1; }
+cp ../dist/Release/GNU-Linux-x86/lfc-analyzer ./ || { echo "Cannot copy ../dist/Release/GNU-Linux-x86/lfc-analyzer to bin directory"; return -1; }
+cd .. || { echo "Error in changing directory to project directory"; return -1; }
 echo "Installation successful"
